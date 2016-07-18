31bf07cf1fb17bf57f4d82f56a93df3b431b80b0
Eric Mey <emey@salesforce.com>
2016-07-18 07:55:09 -0400

    Initial Commit

---
+++ gSend/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  gSend
+//
+//  Created by Eric Mey on 7/18/16.
+//  Copyright © 2016 Steam Rocket Studios, LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
